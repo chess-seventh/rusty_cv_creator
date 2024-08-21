1d36408149962ef2c6be4c528e99bf36a843654d
Chess7th <chess7th@pm.me>
2024-08-21 12:27:12 +0200

    fix: shell.nix to have proper rust version and add direnv ignore

--- shell.nix
+++ shell.nix
@@ -1,24 +1,28 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  moz_overlay = import (builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override {
+    extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];
+  });
+  pkgs = nixpkgs;  # Assign nixpkgs to pkgs for easier reference
 in
-
   pkgs.mkShell {
-    packages = with pkgs; [
-      sqlite
-      texlive.combined.scheme-small
-      diesel-cli
-      postgresql
+    buildInputs = [
+      pkgs.openssl
+      pkgs.rustup
+      ruststable
+      pkgs.cmake
+      pkgs.zlib
+      pkgs.sqlite
+      pkgs.texlive.combined.scheme-small
+      pkgs.diesel-cli
+      pkgs.postgresql
     ];
 
-    # pkgs.mkShellNoCC {
-    #   packages = with pkgs; [
-    #     sqlite
-    #   ];
+    shellHook = ''
+      export OPENSSL_DIR="${pkgs.openssl.dev}"
+      export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+      export DATABASE_URL="postgres://rusty_cv:rusty-cv-01@nixos-01.caracara-palermo.ts.net/db_rusty_cv";
+    '';
+  }
 
-    # DATABASE_URL="sqlite://~/.config/rusty-cv-creator/applications.db";
-    # DATABASE_URL="/home/seventh/.config/rusty-cv-creator/applications.db";
-    DATABASE_URL="postgres://rusty_cv:rusty-cv-01@nixos-01.caracara-palermo.ts.net/db_rusty_cv";
-
-
-}
