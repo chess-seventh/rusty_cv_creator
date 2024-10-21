3c0af5d2e8e369004f8b6f588aa1fe8c17afbcb3
Chess Seventh <chess7th@pm.me>
2024-10-21 16:41:09 +0200

    feat: add the option to view the cv

--- src/cli_structure.rs
+++ src/cli_structure.rs
@@ -1,7 +1,7 @@
+use crate::global_conf::GlobalVars;
+use crate::user_action::{insert_cv, remove_cv, show_cvs};
 use chrono::NaiveDateTime;
 use clap::{Args, Parser, Subcommand};
-use crate::user_action::{insert_cv, remove_cv, show_cvs};
-use crate::global_conf::GlobalVars;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
@@ -12,6 +12,11 @@ pub struct UserInput {
     #[arg(short, long)]
     #[arg(default_value_t = true)]
     pub save_to_database: bool,
+
+    /// Choice to show the generated CV [default: true]
+    #[arg(short, long)]
+    #[arg(default_value_t = true)]
+    pub view_generated_cv: bool,
 
     /// Choice to generate the dirs or CV [default: false]
     #[arg(short, long)]
@@ -68,19 +73,19 @@ pub struct InsertCV {
 
 #[derive(Args, Debug, Clone)]
 #[command(about = "Update a field of a CV in the database", long_about = None)]
-pub struct UpdateCV { }
+pub struct UpdateCV {}
 
 #[derive(Args, Debug, Clone)]
 #[command(about = "Remove a specific CV from the database and remove the directory", long_about = None)]
-pub struct RemoveCV { }
+pub struct RemoveCV {}
 
 #[derive(Args, Debug, Clone)]
 #[command(about = "Show a specific CV that is in the database", long_about = None)]
-pub struct ShowCV { }
+pub struct ShowCV {}
 
 #[derive(Args, Debug, Clone)]
 #[command(about = "List all CVs that are in the database", long_about = None)]
-pub struct ListCV { }
+pub struct ListCV {}
 
 pub fn match_user_action() -> String {
     let input = GlobalVars::get_user_input();
@@ -88,22 +93,18 @@ pub fn match_user_action() -> String {
     // let db_engine = input.db_engine;
     println!("{filters:?}");
     match input.action {
-            UserAction::Insert(_insert) => {
-                insert_cv()
-            }
-            UserAction::Show(_show) => {
-                show_cvs()
-            }
-            UserAction::Remove(_remove) => {
-                remove_cv();
-                "removed the selected cv!".to_string()
-            }
-            UserAction::List(_list) => {
-                todo!()
-            }
-            UserAction::Update(_update) => {
-                todo!();
-            }
+        UserAction::Insert(_insert) => insert_cv(),
+        UserAction::Show(_show) => show_cvs(),
+        UserAction::Remove(_remove) => {
+            remove_cv();
+            "removed the selected cv!".to_string()
+        }
+        UserAction::List(_list) => {
+            todo!()
+        }
+        UserAction::Update(_update) => {
+            todo!();
+        }
     }
 }
 
@@ -121,31 +122,33 @@ pub struct UserFilters {
 }
 
 impl UserFilters {
-     pub fn create(&mut self, user_input: UserInput) -> Self {
-
+    pub fn create(&mut self, user_input: UserInput) -> Self {
         UserFilters {
-            date : self.parse_date(user_input.filter_date),
-            name : user_input.filter_name,
-            job : user_input.filter_job,
-            company : user_input.filter_company,
+            date: self.parse_date(user_input.filter_date),
+            name: user_input.filter_name,
+            job: user_input.filter_job,
+            company: user_input.filter_company,
         }
     }
 
     fn parse_date(&mut self, filter_date: Option<String>) -> Option<NaiveDateTime> {
         filter_date.as_ref()?;
 
-        self.date = match chrono::NaiveDateTime::parse_from_str(&filter_date.clone().unwrap(), "%B") {
+        self.date = match chrono::NaiveDateTime::parse_from_str(&filter_date.clone().unwrap(), "%B")
+        {
             Ok(d) => Some(d),
-            Err(_) => match chrono::NaiveDateTime::parse_from_str(&filter_date.clone().unwrap(), "%b") {
-                Ok(d) => Some(d),
-                Err(_) => match chrono::NaiveDateTime::parse_from_str(&filter_date.unwrap(), "%m") {
+            Err(_) => {
+                match chrono::NaiveDateTime::parse_from_str(&filter_date.clone().unwrap(), "%b") {
                     Ok(d) => Some(d),
-                    _ => panic!("could not parse the date"),
+                    Err(_) => {
+                        match chrono::NaiveDateTime::parse_from_str(&filter_date.unwrap(), "%m") {
+                            Ok(d) => Some(d),
+                            _ => panic!("could not parse the date"),
+                        }
+                    }
                 }
             }
         };
         self.date
-
-    } 
-
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use dotenvy::dotenv;
 use clap::Parser;
+use dotenvy::dotenv;
 
-mod config_parse;
 mod cli_structure;
-mod file_handlers;
-mod helpers;
+mod config_parse;
 mod database;
-mod user_action;
+mod file_handlers;
 mod global_conf;
+mod helpers;
+mod user_action;
 
-use crate::cli_structure::{UserInput, match_user_action};
-use crate::helpers::{view_cv_file, check_if_db_env_is_set_or_set_from_config, fix_home_directory_path};
-use crate::config_parse::{set_global_vars, get_variable_from_config};
-use crate::file_handlers::{create_directory, make_cv_changes_based_on_input, compile_cv};
-
-
+use crate::cli_structure::{match_user_action, UserInput};
+use crate::config_parse::{get_variable_from_config, set_global_vars};
+use crate::file_handlers::{compile_cv, create_directory, make_cv_changes_based_on_input};
+use crate::helpers::{
+    check_if_db_env_is_set_or_set_from_config, fix_home_directory_path, view_cv_file,
+};
 
 fn main() {
     env_logger::init();
@@ -28,21 +28,25 @@ fn main() {
 
     let cv_full_path = match_user_action();
     if !cv_full_path.is_empty() {
-        view_cv_file(&cv_full_path);
+        if user_input.view_generated_cv {
+            view_cv_file(&cv_full_path);
+        } else {
+            println!("CV saved to: {cv_full_path}");
+        }
     }
 }
 
 fn prepare_cv(job_title: &str, company_name: &str, quote: &str) -> String {
-    let cv_template_file = fix_home_directory_path(&get_variable_from_config("cv", "cv_template_file"));
+    let cv_template_file =
+        fix_home_directory_path(&get_variable_from_config("cv", "cv_template_file"));
 
     let created_cv_dir = create_directory(job_title, company_name);
 
-    let destination_cv_file_full_path = fix_home_directory_path(&format!("{created_cv_dir}/{cv_template_file}"));
+    let destination_cv_file_full_path =
+        fix_home_directory_path(&format!("{created_cv_dir}/{cv_template_file}"));
 
     make_cv_changes_based_on_input(job_title, quote, &destination_cv_file_full_path);
     compile_cv(&created_cv_dir, &cv_template_file);
 
     destination_cv_file_full_path
 }
-
-
