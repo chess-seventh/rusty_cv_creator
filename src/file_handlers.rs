590af8df83aeb949aa2286258373277d26c097fd
Chess Seventh <chess7th@pm.me>
2025-08-20 12:02:13 +0200

    feat: Refactor directory paths in file tests
    - Standardize base directory paths across different test cases.
    - Introduce ignore markers for selective local testing.

--- src/file_handlers.rs
+++ src/file_handlers.rs
@@ -290,17 +290,18 @@ mod tests {
 
     #[test]
     fn test_directory_validity() {
-        let result = check_dir_exists("/home/seventh/");
+        let result = check_dir_exists("/home/");
         assert!(result);
     }
 
     #[test]
     fn test_directory_is_invalid() {
-        let result = check_dir_exists("/home/seventh/doesnotexist/");
+        let result = check_dir_exists("/home/doesnotexist/");
         assert!(!result);
     }
 
     #[test]
+    #[ignore = "Used for local testing"]
     fn test_file_validity() {
         let result = check_file_exists(
             "/home/seventh/.config/rusty-cv-creator/",
@@ -310,11 +311,9 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "Used for local testing"]
     fn test_file_validity_is_invalid() {
-        let result = check_file_exists(
-            "/home/seventh/.config/rusty-cv-creator/",
-            "doesnotexist.ini",
-        );
+        let result = check_file_exists("/home/root/.config/rusty-cv-creator/", "doesnotexist.ini");
         assert!(!result);
     }
 
